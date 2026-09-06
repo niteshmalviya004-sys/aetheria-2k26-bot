1e91f8e456166bf33b8fa0b3aaab72b57ccb38c0
niteshmalviya004-sys <niteshmalviya004@gmail.com>
2026-09-06 19:55:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason, Browsers } = require('@whiskeysockets/baileys');
 const http = require('http');
 
 // Render Port Binding
